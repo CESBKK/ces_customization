762748d898f24055ce5ae2ea97caeb42aabbc728
CESBKK <cescoltdbkk@gmail.com>
2025-01-21 05:26:50 +0000

    Add Sales custom and fixture hook

--- ces_customization/hooks.py
+++ ces_customization/hooks.py
@@ -9,6 +9,15 @@ app_license = "mit"
 # ------------------
 
 # required_apps = []
+required_apps = ["erpnext"]
+
+fixtures = [
+    {
+        "dt": "Custom Field",
+        "filters": [["fieldname", "like", "ces%"]]
+    },
+]
+
 
 # Each item in the list will be shown as an app in the apps page
 # add_to_apps_screen = [
