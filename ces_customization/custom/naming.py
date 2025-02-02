0b4d327870b964d5eac5a7b5984717b0df8e89a3
CESBKK <cescoltdbkk@gmail.com>
2025-02-02 11:22:14 +0000

    remove PD from variable name

--- ces_customization/custom/naming.py
+++ ces_customization/custom/naming.py
@@ -1,79 +1,73 @@
 from frappe.model.naming import determine_consecutive_week_number
 from frappe.utils import (
-    get_datetime
+    getdate,
+    # get_datetime
 )
 
 
 def parse_naming_series_variable(doc, variable):
-    if variable == 'CES-PD-YY' and doc:
-        date = doc.get('posting_date')
-        r = populate_serie(date=date)
+    date = getdate()
+    date = doc.get("posting_date") or doc.get("transaction_date") or doc.get("date")
+    if isinstance(date, str):
+        date = getdate(date)
+
+    r = populate_serie(date=date)
+    r_be = populate_serie(date=date, year_type='BE')
+    if variable == 'CES-YY' and doc:
         return r['yy']
 
-    if variable == 'CES-PD-YYYY' and doc:
-        date = doc.get('posting_date')
-        r = populate_serie(date=date)
+    if variable == 'CES-YYYY' and doc:
         return r['yyyy']
 
-    if variable == 'CES-PD-YYBE' and doc:
-        date = doc.get('posting_date')
-        r = populate_serie(date=date, year_type='BE')
-        return r['yy']
+    if variable == 'CES-YY-BE' and doc:
+        return r_be['yy']
 
-    if variable == 'CES-PD-YYYYBE' and doc:
-        date = doc.get('posting_date')
-        r = populate_serie(date=date, year_type='BE')
-        return r['yyyy']
+    if variable == 'CES-YYYY-BE' and doc:
+        return r_be['yyyy']
 
-    if variable == 'CES-PD-WW' and doc:
-        date = doc.get('posting_date')
-        r = populate_serie(date=date)
+    if variable == 'CES-WW' and doc:
         return r['ww']
 
-    if variable == 'CES-PD-MM' and doc:
-        date = doc.get('posting_date')
-        r = populate_serie(date=date)
+    if variable == 'CES-MM' and doc:
         return r['mm']
 
-    if variable == 'CES-PD-DD' and doc:
-        date = doc.get('posting_date')
-        r = populate_serie(date=date)
+    if variable == 'CES-DD' and doc:
         return r['dd']
 
-    if variable == 'CES-TD-YY' and doc:
-        date = doc.get('transaction_date')
-        r = populate_serie(date=date)
-        return r['yy']
+    # if variable == 'CES-TD-YY' and doc:
+    #     date = doc.get('transaction_date')
+    #     r = populate_serie(date=date)
+    #     return r['yy']
 
-    if variable == 'CES-TD-YYYY' and doc:
-        date = doc.get('transaction_date')
-        r = populate_serie(date=date)
-        return r['yyyy']
+    # if variable == 'CES-TD-YYYY' and doc:
+    #     date = doc.get('transaction_date')
+    #     r = populate_serie(date=date)
+    #     return r['yyyy']
 
-    if variable == 'CES-TD-YYBE' and doc:
-        date = doc.get('transaction_date')
-        r = populate_serie(date=date, year_type='BE')
-        return r['yy']
+    # if variable == 'CES-TD-YYBE' and doc:
+    #     date = doc.get('transaction_date')
+    #     r = populate_serie(date=date, year_type='BE')
+    #     return r['yy']
 
-    if variable == 'CES-TD-YYYYBE' and doc:
-        date = doc.get('transaction_date')
-        r = populate_serie(date=date, year_type='BE')
-        return r['yyyy']
+    # if variable == 'CES-TD-YYYYBE' and doc:
+    #     date = doc.get('transaction_date')
+    #     r = populate_serie(date=date, year_type='BE')
+    #     return r['yyyy']
 
-    if variable == 'CES-TD-WW' and doc:
-        date = doc.get('transaction_date')
-        r = populate_serie(date=date)
-        return r['ww']
+    # if variable == 'CES-TD-WW' and doc:
+    #     date = doc.get('transaction_date')
+    #     r = populate_serie(date=date)
+    #     return r['ww']
 
-    if variable == 'CES-TD-MM' and doc:
-        date = doc.get('transaction_date')
-        r = populate_serie(date=date)
-        return r['mm']
+    # if variable == 'CES-TD-MM' and doc:
+    #     date = doc.get('transaction_date')
+    #     r = populate_serie(date=date)
+    #     return r['mm']
 
-    if variable == 'CES-TD-DD' and doc:
-        date = doc.get('transaction_date')
-        r = populate_serie(date=date)
-        return r['dd']
+    # if variable == 'CES-TD-DD' and doc:
+    #     date = doc.get('transaction_date')
+    #     r = populate_serie(date=date)
+    #     return r['dd']
 
     if variable == 'CES-PMT-TYPE' and doc:
         pmt_type = doc.get('payment_type')
@@ -86,7 +80,7 @@ def parse_naming_series_variable(doc, variable):
 def populate_serie(date, year_type='AD'):
     # make sure that the supply posting date is in datetime type
     # sometimes Frappe just return datetime as str
-    target_date = get_datetime(date)
+    target_date = getdate(date)
     result = {}
     if year_type == 'AD':
         result['yyyy'] = str(target_date.year)
